90312f0ed315b388d47cc09013048f9b0dcd968f
Justin Warner <warner.73@wright.edu>
2018-07-25 02:43:44 -0700

    Update Lab01.test.py

--- labFiles/introCS/Lab01.test.py
+++ labFiles/introCS/Lab01.test.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-import studentFile
+import Lab01
 import unittest
 import sys
 import io
@@ -11,18 +11,18 @@ class UnitTests(unittest.TestCase):
     @patch("random.randint")
     def test_cards(self, mockRandInt):
         mockRandInt.return_value = 1
-        result = studentFile.cards()
+        result = Lab01.cards()
         self.assertEqual(result, "2 Clubs")
 
     #arguments passing in
     def test_birthday_song(self):
         song = 'happy birthday to you, happy birthday to you, happy birthday dear TEALS, happy birthday to you'
-        self.assertAlmostEqual(studentFile.birthday_song('TEALS'), song)
+        self.assertAlmostEqual(Lab01.birthday_song('TEALS'), song)
 
     #printing
     @patch('sys.stdout.write')
     def test_hello_world(self, mock_print):
-        studentFile.hello_world()
+        Lab01.hello_world()
         mock_print.assert_any_call('Hello World')
 
 if __name__ == '__main__':
